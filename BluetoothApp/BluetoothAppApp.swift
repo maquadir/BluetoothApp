b4f5e640488cb9be8cc03fcfc64ffb8fe68f6fa6
adeemad <adeem@adabdigital.com>
2023-04-16 13:18:46 +1000

    Initial Commit

---
+++ BluetoothApp/BluetoothAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothAppApp.swift
+//  BluetoothApp
+//
+//  Created by Muhammad Adeemul Khader on 16/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
